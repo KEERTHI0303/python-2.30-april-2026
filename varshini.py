f3fd76f7dc151e8f7795e0022b8798272837a379
KEERTHI0303 <keerthiparam0303@gmail.com>
2026-04-21 17:00:25 +0530

    Add files via upload

---
+++ varshini.py
@@ -0,0 +1,46 @@
+'''a=10
+print(a)
+name='keerthi'
+print(name)
+firstname="karthi"#FULLY lower
+print(firstname)
+LASTNAME="keyan"#UPPER CASE
+print(LASTNAME)
+print(firstname+LASTNAME)
+nAmE="APPLE"#mixed case
+print(nAmE)
+a1=56
+print(a1)'''
+#2dfd=88
+#if=90
+#date of birth="12-12-12"
+"""date_of_birth="12-12-12"
+print(date_of_birth)"""
+
+
+#basic data type => one var contains one data and one data type
+#core data type = one var contains more than one data and one data type
+a=10,"hi"
+print(a)
+print(type(a))
+a='hi'
+print(a)
+print(type(a))
+
+a=19.67
+print(type(a))
+
+s=(2+6j)
+print(s)
+print(type(s))
+
+
+
+
+
+
+
+
+
+
+
